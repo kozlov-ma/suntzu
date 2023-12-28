0785797210db6ed811266506faff9852f9a7bca6
Mikhail Kozlov <wmikkoz@icloud.com>
2023-12-28 18:17:25 +0500

    Select board size

--- game_gui.py
+++ game_gui.py
@@ -2,6 +2,8 @@ import itertools
 from functools import cache
 
 import pygame
+import pygame_widgets as pw
+from pygame_widgets.button import Button
 
 from board import Position, Side, Stone, Board
 from game import Move, Pass, Game
@@ -12,13 +14,14 @@ BLACK = (0, 0, 0)
 
 
 @cache
-def init_graphics():
+def init_graphics(w=WIDTH, h=HEIGHT):
+    pygame.init()
     pygame.font.init()
     background_image = pygame.image.load('background.jpeg')
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen = pygame.display.set_mode((w, h))
 
     pygame.display.set_caption("suntzu -- Game of Go")
-    screen.blit(background_image, (-320, -180))
+    screen.blit(background_image, (0, 0))
 
     return screen, background_image
 
@@ -28,7 +31,7 @@ def draw_board(screen: pygame.Surface, bg_image: pygame.Surface, board: Board):
     verticals = [(WIDTH * 0.1) + i * interval for i in range(board.n)]
     horizontals = [(HEIGHT * 0.1) + i * interval for i in range(board.n)]
 
-    screen.blit(bg_image, (-320, -180))
+    screen.blit(bg_image, (0, 0))
     for vertical in verticals:
         pygame.draw.line(screen, BLACK, (vertical, HEIGHT * 0.1), (vertical, HEIGHT * 0.9))
 
@@ -48,16 +51,48 @@ def draw_board(screen: pygame.Surface, bg_image: pygame.Surface, board: Board):
     pygame.display.update()
 
 
+def get_n() -> int:
+    screen, _ = init_graphics(750, 350)
+    buttons = [pw.button.Button(screen, 100, 100, 150, 150, text='9x9', fontSize=50, margin=20, inactiveColour=WHITE,
+        pressedColour=(0, 255, 0), radius=20),
+        pw.button.Button(screen, 300, 100, 150, 150, text='13x13', fontSize=50, margin=20, inactiveColour=WHITE,
+            pressedColour=(0, 255, 0), radius=20),
+        pw.button.Button(screen, 500, 100, 150, 150, text='19x19', fontSize=50, margin=20, inactiveColour=WHITE,
+            pressedColour=(0, 255, 0), radius=20), ]
+
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                x, y = pygame.mouse.get_pos()
+                if abs(x - 100) <= 150 and abs(y - 100) <= 150:
+                    return 9
+                elif abs(x - 300) <= 150 and abs(y - 300) <= 150:
+                    return 13
+                elif abs(x - 500) <= 150 and abs(y - 500) <= 150:
+                    return 19
+                break
+
+        for b in buttons:
+            b.listen(events)
+            b.draw()
+            pygame.display.update()
+
+
 def hot_seat():
-    game = Game()
+    n = get_n()
+
+    game = Game(board=Board(n))
     board = game.board
+
+    screen, bg = init_graphics()
 
     player1 = game.create_human_player(Side.Black)
     player2 = game.create_human_player(Side.White)
 
     players = itertools.cycle([player1, player2])
-
-    screen, bg = init_graphics()
 
     draw_board(screen, bg, board)
     while True:
